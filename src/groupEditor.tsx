68a4732e1d7bef1728f9e5ae082a13f3e95387ba
b2pacific <b2pacific@gmail.com>
2021-05-26 19:12:18 +0530

    Added some styling

--- src/groupEditor.tsx
+++ src/groupEditor.tsx
@@ -1,19 +1,31 @@
 import * as React from "react";
 import SyncingEditor from "./syncingEditor";
 
-// {data.match.params.id}
-
 const GroupEditor = (data: any) => {
-    const [value, setValue] = React.useState("");
-    const [flag, setflag] = React.useState(false);
+  const [value, setValue] = React.useState("");
+  const [flag, setflag] = React.useState(false);
   return (
     <div>
-        { flag ? (<SyncingEditor groupId={value} />) : 
-            (<div><button onClick={()=> setflag(true) }>Create New file</button>
-            <input type="text" onChange={(e) => setValue(e.target.value)} />
-            <button onClick={() => setflag(true)}>Open</button></div>)
-        }
-      
+      {flag ? (
+        <SyncingEditor groupId={value} />
+      ) : (
+        <div
+          style={{
+            display: "grid",
+            height: "500px",
+            marginTop: "100px",
+            paddingLeft: "40%",
+            gridTemplateColumns: "300px",
+            gridTemplateRows: "100px 50px 50px",
+            alignItems: "center",
+            justifyItems: "center",
+          }}
+        >
+          <button onClick={() => setflag(true)}>Create New file</button>
+          <input type="text" onChange={(e) => setValue(e.target.value)} />
+          <button onClick={() => setflag(true)}>Open</button>
+        </div>
+      )}
     </div>
   );
 };
